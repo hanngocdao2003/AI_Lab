cf745783c0c8de8066cec9381c3771db4adaeb26
hanngocdao2003 <21130345@st.hcmuaf.edu.vn>
2023-11-06 23:22:44 +0700

    update lab 5

--- src/lab5/AStarSearch.java
+++ src/lab5/AStarSearch.java
@@ -27,10 +27,14 @@ public class AStarSearch implements IPuzzleAlgo {
 				for (Node child : model.getSuccessors(currentNode)) {
 					int tentativeG = currentNode.getG() + 1;
 
-					if (!explored.contains(child) && (!frontier.contains(child) || tentativeG < child.getG())) {
+					if (!explored.contains(child) && !frontier.contains(child)) {
+						child.setH(model.computeH2(currentNode));
+						child.setG(currentNode.getG() + 1);
+						frontier.add(child);
+					} else if (frontier.contains(child) && child.getG() > tentativeG) {
 						frontier.remove(child);
 						child.setG(tentativeG);
-						child.setH(model.computeH2(child));
+						child.setH(model.computeH2(currentNode));
 						frontier.add(child);
 					}
 				}
